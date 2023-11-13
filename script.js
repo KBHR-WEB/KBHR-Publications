b5f3703b14b0278d669bfd090f9cc502b9f61cec
KBHR-WEB <147394940+KBHR-WEB@users.noreply.github.com>
2023-11-13 12:03:26 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,3 @@
-// Sélectionnez le bouton du menu et le contenu du menu
-var dropdownButton = document.querySelector('.dropbtn');
-var dropdownContent = document.querySelector('.dropdown-content');
-
-// Ajoutez un gestionnaire d'événement au bouton pour ouvrir/fermer le menu
-dropdownButton.addEventListener('click', function() {
-    // Vérifiez si le menu est actuellement affiché
-    if (dropdownContent.style.display === 'block') {
-        // Si c'est le cas, masquez-le
-        dropdownContent.style.display = 'none';
-    } else {
-        // Sinon, affichez-le
-        dropdownContent.style.display = 'block';
-    }
-});
-
-// Ajoutez un gestionnaire d'événement pour fermer le menu lorsqu'on clique en dehors
-window.addEventListener('click', function(event) {
-    if (!event.target.matches('.dropbtn')) {
-        dropdownContent.style.display = 'none';
-    }
-});
-
-
-
 document.addEventListener("DOMContentLoaded", function() {
     const acheterButtons = document.querySelectorAll('.acheter');
     const modal = document.getElementById('modal');
@@ -61,39 +36,4 @@ document.addEventListener("DOMContentLoaded", function() {
     waveBtn.addEventListener('click', function() {
         // Redirection vers la page de paiement Wave Money
     });
-});
-
-profileButton.addEventListener('click', () => {
-    // Gérer l'accès au profil de l'utilisateur
-    alert("Rediriger vers la page de profil de l'utilisateur.");
-});
-
-
-// Sélectionnez le bouton du menu et le contenu du menu
-var dropdownButton = document.querySelector('.dropbtn');
-var dropdownContent = document.querySelector('.dropdown-content');
-
-// Ajoutez un gestionnaire d'événement au bouton pour ouvrir/fermer le menu
-dropdownButton.addEventListener('click', function() {
-    // Vérifiez si le menu est actuellement affiché
-    if (dropdownContent.style.display === 'block') {
-        // Si c'est le cas, masquez-le
-        dropdownContent.style.display = 'none';
-    } else {
-        // Sinon, affichez-le
-        dropdownContent.style.display = 'block';
-    }
-});
-
-// Ajoutez un gestionnaire d'événement pour fermer le menu lorsqu'on clique en dehors
-window.addEventListener('click', function(event) {
-    if (!event.target.matches('.dropbtn')) {
-        dropdownContent.style.display = 'none';
-    }
-});
-
-
-profileButton.addEventListener('click', () => {
-    // Gérer l'accès au profil de l'utilisateur
-    alert("Rediriger vers la page de profil de l'utilisateur.");
 });
