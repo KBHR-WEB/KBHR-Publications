db0598af82840d52a01904f16c9c15a2bb915d94
KBHR-WEB <147394940+KBHR-WEB@users.noreply.github.com>
2023-11-13 12:04:36 +0000

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,39 +1,28 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const acheterButtons = document.querySelectorAll('.acheter');
-    const modal = document.getElementById('modal');
-    const closeBtn = document.querySelector('.close');
+// Sélectionnez le bouton du menu et le contenu du menu
+var dropdownButton = document.querySelector('.dropbtn');
+var dropdownContent = document.querySelector('.dropdown-content');
 
-    acheterButtons.forEach(button => {
-        button.addEventListener('click', function() {
-            modal.style.display = 'block';
-            const livreId = button.getAttribute('data-livre');
-            // Utilisez livreId pour déterminer quel livre a été choisi
-        });
-    });
+// Ajoutez un gestionnaire d'événement au bouton pour ouvrir/fermer le menu
+dropdownButton.addEventListener('click', function() {
+    // Vérifiez si le menu est actuellement affiché
+    if (dropdownContent.style.display === 'block') {
+        // Si c'est le cas, masquez-le
+        dropdownContent.style.display = 'none';
+    } else {
+        // Sinon, affichez-le
+        dropdownContent.style.display = 'block';
+    }
+});
 
-    closeBtn.addEventListener('click', function() {
-        modal.style.display = 'none';
-    });
+// Ajoutez un gestionnaire d'événement pour fermer le menu lorsqu'on clique en dehors
+window.addEventListener('click', function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        dropdownContent.style.display = 'none';
+    }
+});
 
-    // Gérer les options de paiement
-    const visaBtn = document.getElementById('visa');
-    const orangeBtn = document.getElementById('orange');
-    const mtnBtn = document.getElementById('mtn');
-    const waveBtn = document.getElementById('wave');
 
-    visaBtn.addEventListener('click', function() {
-        // Redirection vers la page de paiement Visa
-    });
-
-    orangeBtn.addEventListener('click', function() {
-        // Redirection vers la page de paiement Orange Money
-    });
-
-    mtnBtn.addEventListener('click', function() {
-        // Redirection vers la page de paiement MTN Mobile Money
-    });
-
-    waveBtn.addEventListener('click', function() {
-        // Redirection vers la page de paiement Wave Money
-    });
+profileButton.addEventListener('click', () => {
+    // Gérer l'accès au profil de l'utilisateur
+    alert("Rediriger vers la page de profil de l'utilisateur.");
 });
